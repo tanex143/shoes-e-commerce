ec5e7371ac8c926b634ee6ce3e227880c8f8b578
tanex143 <tanexjunior29@gmail.com>
2021-01-07 14:51:45 +0800

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
+import Navbar from './components/navbar';
+import Products from './components/products';
+
 const App = () => {
   return (
-    <div className='App'>
-      <h1 className='p-10 bg-gradient-to-r from-coolgray-500 via-bluegray-500 to-gray-500 text-center'>
-        Gradient Gray
-      </h1>
-      <h1 className='p-10 bg-bluegray-500 text-center'>Blue Gray</h1>
-      <h1 className='p-10 bg-coolgray-500 text-center'>Cool Gray</h1>
-      <h1 className='p-10 bg-truegray-500 text-center'>True Gray</h1>
-      <h1 className='p-10 bg-gray-500 text-center'>Gray</h1>
-    </div>
+    <>
+      <Navbar />
+      <Products />
+    </>
   );
 };
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faShoppingBag,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  return (
+    <div className='py-3 bg-teal-300'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between'>
+          <div className='flex'>
+            <img src='logo.png' alt='logo' className='mr-1 w-16' />
+            <h1 className='text-2xl font-semibold tracking-wide logo-font'>
+              SHOES
+            </h1>
+          </div>
+          <div className='flex'>
+            <h1 className='mr-1 text-2xl font-semibold'>Welcome!</h1>
+            <h1 className='text-2xl font-semibold'>TaneX</h1>
+            <div className='ml-10 px-2'>
+              <FontAwesomeIcon
+                icon={faShoppingCart}
+                className='text-3xl cursor-pointer text-gray-700'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
