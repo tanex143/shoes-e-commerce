cbb58835f8ae3aea92352d35a1c38199a339544a
tanex143 <tanexjunior29@gmail.com>
2021-01-07 13:26:46 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='p-10 bg-gradient-to-r from-coolgray-500 via-bluegray-500 to-gray-500 text-center'>
+        Gradient Gray
+      </h1>
+      <h1 className='p-10 bg-bluegray-500 text-center'>Blue Gray</h1>
+      <h1 className='p-10 bg-coolgray-500 text-center'>Cool Gray</h1>
+      <h1 className='p-10 bg-truegray-500 text-center'>True Gray</h1>
+      <h1 className='p-10 bg-gray-500 text-center'>Gray</h1>
     </div>
   );
-}
+};
 
 export default App;
